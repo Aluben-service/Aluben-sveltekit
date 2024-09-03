a4fff23af0bddff30abbecbc2aa73c04b3f1c952
Google <138321129+Coding4Hours@users.noreply.github.com>
2024-09-02 17:37:55 -0700

    todos

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,8 @@ export default {
 	kit: {
 		// default options are shown
 		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
 			edge: false,
 
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
 			split: false
 		})
 	}
