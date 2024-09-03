c4ffadf0fb8f5e8f190ff0a2cea383e03f2678da
Google <138321129+Coding4Hours@users.noreply.github.com>
2024-09-02 17:52:51 -0700

    todos

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,15 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-
-const dev = process.env.NODE_ENV === 'development';
 
 export default {
-	preprocess: preprocess(),
 	kit: {
-		// Configure paths to support both platforms
-		paths: {
-			base: dev ? '' : '', // You can set a base path if needed
-			assets: dev ? '' : '',
-		},
 		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: null, // If you're using SvelteKit's routing with dynamic pages, consider setting a fallback like 'index.html'
+			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		prerender: {
-			crawl: true,
-			enabled: true,
-			entries: ['*'],
-		}
-	}
-};
-
-/*import adapter from '@sveltejs/adapter-netlify';
-
-export default {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			edge: false,
-
-			split: false
 		})
 	}
-};*/
+};
