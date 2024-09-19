8f88a4dd27692db48628184f501a7ee1b0ff8150
I don’t codddde <120886940+asc2563@users.noreply.github.com>
2024-09-18 19:14:34 -0700

    Update +page.svelte

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -2,8 +2,6 @@
 	import { onMount } from "svelte";
 	import "../../app.css";
 	import Navbar from "$lib/components/Navbar.svelte";
- import Head from "$lib/components/Head.svelte";
-
 	onMount(() => {
 		// Load external scripts
 		const script1 = document.createElement("script");
@@ -29,7 +27,27 @@
 	});
 </script>
 
-<Head />
+<svelte:head>
+	<meta charset="utf-8" />
+	<link rel="canonical" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="stylesheet" href="assets/css/gamecard.css" />
+	<!--<link rel="stylesheet" href="assets/css/styles.css" />-->
+	<meta
+		name="description"
+		content="A good proxy with custom unblockers (that are completely different proxies) and a proxy list built in games it's amazing"
+	/>
+	<meta name="viewport" content="width=device-width" />
+	<title>Error Page | Ebay</title>
+	<link
+		rel="icon"
+		type="image/x-icon"
+		href="https://www.ebay.com/favicon.ico"
+	/>
+	<script src="/assets/lib/fontawesome.js" defer></script>
+	<script src="/assets/lib/sweetAlerts.js" defer></script>
+	<link rel="manifest" href="manifest.json" />
+</svelte:head>
 
 <Navbar />
 
