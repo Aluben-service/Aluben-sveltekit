a05578969e194fb156b3d313158862703cc4cbdd
asc2563 <allistersaintclair@outlook.com>
2024-09-22 14:27:21 -0700

    modified +page.svelte in settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -83,84 +83,82 @@
 <h1 style="font-size: 2em;">Settings</h1>
 
 <div style="display: gride;">
-
-<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
-	<div>
-		<div class="uk-card uk-card-body uk-card-default">
-			<h3 class="uk-card-title">Tab Cloak</h3>
-			<p style="font-size: 11px;">
-				This will disguise your tabs and make them look like Clever,
-				Google Classroom, Schoology, etc.
-			</p>
-			<select bind:value={currentCloak} on:change={set_cloak}>
-				<option selected disabled value="N/A">Choose one</option>
-				<option value="Google">Google</option>
-				<option value="Drive">Drive</option>
-				<option value="Classroom">Google Classroom</option>
-				<option value="Schoology">Schoology</option>
-				<option value="Gmail">Gmail</option>
-				<option value="Clever">Clever</option>
-				<option value="Khan">Khan Academy</option>
-				<option value="Ebay">Ebay</option>
-			</select>
-			<br />
-			<input
-				type="text"
-				on:input={customcloak}
-				placeholder="Enter a custom tab name"
-			/>
-			<br />
-			<input
-				type="text"
-				on:input={customcloakfavicon}
-				placeholder="Enter a custom tab favicon URL"
-			/>
+	<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
+		<div>
+			<div class="uk-card uk-card-body uk-card-default">
+				<h3 class="uk-card-title">Tab Cloak</h3>
+				<p style="font-size: 11px;">
+					This will disguise your tabs and make them look like Clever,
+					Google Classroom, Schoology, etc.
+				</p>
+				<select bind:value={currentCloak} on:change={set_cloak}>
+					<option selected disabled value="N/A">Choose one</option>
+					<option value="Google">Google</option>
+					<option value="Drive">Drive</option>
+					<option value="Classroom">Google Classroom</option>
+					<option value="Schoology">Schoology</option>
+					<option value="Gmail">Gmail</option>
+					<option value="Clever">Clever</option>
+					<option value="Khan">Khan Academy</option>
+					<option value="Ebay">Ebay</option>
+				</select>
+				<br />
+				<input
+					type="text"
+					on:input={customcloak}
+					placeholder="Enter a custom tab name"
+				/>
+				<br />
+				<input
+					type="text"
+					on:input={customcloakfavicon}
+					placeholder="Enter a custom tab favicon URL"
+				/>
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
-	<div>
-		<div class="uk-card uk-card-body uk-card-default">
-			<h3 class="uk-card-title">Passcode</h3>
-			<p style="font-size: 11px;">
-				Click the button below to change your passcode!
-			</p>
-			<button class="bounce-light" on:click={() =>passcodechange}
-				>Change</button
-			>
-<!-- THIS IS FOR A REASON !!!!! -->
-			<!-- This is a bypass to access the js like we are in HTML because normally js doesnt leak into svelte-->
+	<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
+		<div>
+			<div class="uk-card uk-card-body uk-card-default">
+				<h3 class="uk-card-title">Passcode</h3>
+				<p style="font-size: 11px;">
+					Click the button below to change your passcode!
+				</p>
+				<button class="bounce-light" on:click={passcodechange}
+					>Change</button
+				>
+				<!-- THIS IS FOR A REASON !!!!! -->
+				<!-- This is a bypass to access the js like we are in HTML because normally js doesnt leak into svelte-->
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
-	<div>
-		<div class="uk-card uk-card-body uk-card-default">
-			<h3 class="uk-card-title">Themes</h3>
-			<p style="font-size: 11px;">Are your eyes hurting?</p>
-			<select bind:value={selectedTheme} id="themes">
-				<option value="N/A" disabled>Choose a theme</option>
-				<option value="Default">Default</option>
-				<option value="bannana_split">Bannana Split</option>
-			</select>
-			<button on:click={set_theme}>Save</button>
+	<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
+		<div>
+			<div class="uk-card uk-card-body uk-card-default">
+				<h3 class="uk-card-title">Themes</h3>
+				<p style="font-size: 11px;">Are your eyes hurting?</p>
+				<select bind:value={selectedTheme} id="themes">
+					<option value="N/A" disabled>Choose a theme</option>
+					<option value="Default">Default</option>
+					<option value="bannana_split">Bannana Split</option>
+				</select>
+				<button on:click={set_theme}>Save</button>
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
-	<div>
-		<div class="uk-card uk-card-body uk-card-default">
-			<h1>WIP doesn't currently work</h1>
-			<h3 class="uk-card-title">About Blank</h3>
-			<p style="font-size: 11px;">No history?</p>
-			<button on:click={toggleab}>Toggle</button>
+	<div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
+		<div>
+			<div class="uk-card uk-card-body uk-card-default">
+				<h1>WIP doesn't currently work</h1>
+				<h3 class="uk-card-title">About Blank</h3>
+				<p style="font-size: 11px;">No history?</p>
+				<button on:click={toggleab}>Toggle</button>
+			</div>
 		</div>
 	</div>
-</div>
-
 </div>
 
 <h3>Panic key/ (no https:// or http://)</h3>
