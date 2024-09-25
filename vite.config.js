a5840e3e6706b3a0d8ceaff90d45fe21e4add19f
asc2563 <allistersaintclair@outlook.com>
2024-09-24 18:19:26 -0700

    changed 9 files

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,24 @@
-import { fileURLToPath } from 'url';
-import { join } from 'path';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import { partytownVite } from '@builder.io/partytown/utils';
-import { ChemicalVitePlugin } from 'chemicaljs';
+import { fileURLToPath } from "url";
+import { join } from "path";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { partytownVite } from "@builder.io/partytown/utils";
+import { ChemicalVitePlugin } from "chemicaljs";
 
 // Convert module URL to path in ES modules
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    partytownVite({
-      dest: join(__dirname, 'static', '~partytown'),
-    }),
-<<<<<<< HEAD
-    ChemicalVitePlugin()
-=======
-    ChemicalVitePlugin({meteor: false})
->>>>>>> 1d870e77f60955671deb4a9f0c66584015a00e42
-  ],
-  build: {
-    rollupOptions: {
-      external: ['express'],  // External dependencies
-    },
-  },
+	plugins: [
+		sveltekit(),
+		partytownVite({
+			dest: join(__dirname, "static", "~partytown"),
+		}),
+		ChemicalVitePlugin({ meteor: false }),
+	],
+	build: {
+		rollupOptions: {
+			external: ["express"], // External dependencies
+		},
+	},
 });
