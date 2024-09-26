561a296beb37a7cc9469bb74ee0c1785eab77436
asc2563 <allistersaintclair@outlook.com>
2024-09-26 15:43:29 -0700

    added 13 files

---
+++ static/mayberemoved/index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+
+form.addEventListener("submit", async (event) => {
+	event.preventDefault();
+
+	try {
+		await registerSW();
+	} catch (err) {
+		error.textContent = "Failed to register service worker.";
+		errorCode.textContent = err.toString();
+		throw err;
+	}
+
+	const url = search(address.value, searchEngine.value);
+
+	let frame = document.getElementById("uv-frame");
+	frame.style.display = "block";
+	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+	/*
+    let game = {
+      url: __uv$config.prefix + __uv$config.encodeUrl(url),
+      img: '/web.png',
+      name: "Pr0xied Website",
+      desc: "Pr0xied Website"
+    }
+    localforage.setItem("currentgame", game);
+    location.href = "/play"*/
+});
---
+++ static/mayberemoved/register-sw.js
@@ -0,0 +1,38 @@
+"use strict";
+/**
+ * Distributed with Ultraviolet and compatible with most configurations.
+ */
+const stockSW = "/ultraviolet/sw.js";
+
+/**
+ * List of hostnames that are allowed to run serviceworkers on http://
+ */
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
+
+/**
+ * Global util
+ * Used in 404.html and index.html
+ */
+const registerSW = async () => {
+	if (!navigator.serviceWorker) {
+		if (
+			location.protocol !== "https:" &&
+			!swAllowedHostnames.includes(location.hostname)
+		)
+			throw new Error(
+				"Service workers cannot be registered without https.",
+			);
+
+		throw new Error("Your browser doesn't support service workers.");
+	}
+
+	await navigator.serviceWorker.register(stockSW, {
+		scope: __uv$config.prefix,
+	});
+
+	// This is the line you change to change the wisp server (essential for static hosting ofc)
+	//let wispUrl = "wss://phantom.lol/wisp/";
+	let wispUrl = "wss://wispserver.onrender.com/wisp/";
+	BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
+	//BareMux.SetTransport("CurlMod.LibcurlClient", { wisp: wispUrl });
+};
