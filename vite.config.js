b684fc2fb403a7c8931a434c14babb8cde20aa88
asc2563 <allistersaintclair@outlook.com>
2024-09-26 15:03:38 -0700

    modified vite.config.js in .

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { join } from "node:path";
+import { join } from "path";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { partytownVite } from "@builder.io/partytown/utils";
